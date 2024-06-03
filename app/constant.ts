e8e5096dbf663da13c29b3b419e5726921d738ec
xxhdtd <132115083+xxhdtd@users.noreply.github.com>
2024-06-03 23:16:01 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -163,9 +163,9 @@ const anthropicModels = [
   "claude-instant-1.2",
   "claude-2.0",
   "claude-2.1",
-  "claude-3-sonnet-20240229",
-  "claude-3-opus-20240229",
-  "claude-3-haiku-20240307",
+  // "claude-3-sonnet-20240229",
+  // "claude-3-opus-20240229",
+  // "claude-3-haiku-20240307",
 ];
 
 export const DEFAULT_MODELS = [
